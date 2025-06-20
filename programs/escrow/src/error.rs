30dcdde7665597e65d6af55267a96bb6a64fd46f
Harneet <harneetsahi8@gmail.com>
2025-06-20 13:29:21 -0500

    add: errors

--- programs/escrow/src/error.rs
+++ programs/escrow/src/error.rs
@@ -2,6 +2,12 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
-}
+    #[msg("Failed to withdraw tokens from the vault")]
+    FailedVaultWithdrawal,
+
+    #[msg("Failed to close the vault")]
+    FailedVaultClosure,
+
+    #[msg("Insufficient balance in taker's account")]
+    InsufficientTakerBalance,
+    }
